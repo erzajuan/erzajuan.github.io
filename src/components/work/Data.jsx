9d61c46cd55c04f69a7d47bed0a157685ada8a68
erzajuan <direkal2@gmail.com>
2023-07-08 17:22:23 +0700

    Update 8 Juli 2023

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,11 +1,19 @@
 import work1 from "../../assets/work1.png"
 import work2 from "../../assets/work2.png"
 import work3 from "../../assets/work3.png"
-import work4 from "../../assets/work4.jpg"
+import work4 from "../../assets/work4.avif"
 
 export const projectsData = [
   {
     id: 1,
+    image: work4,
+    title: "REST API Template",
+    category: ["web"],
+    link: "https://github.com/erzajuan/REST-API-Templating",
+    stack: "Node.JS, Express.JS"
+  },
+  {
+    id: 2,
     image: work1,
     title: "Bali Rent Website & App",
     category: ["fullstack", "web", "app"],
@@ -13,7 +21,7 @@ export const projectsData = [
     stack: "Node.JS, React.JS, Flutter"
   },
   {
-    id: 2,
+    id: 3,
     image: work2,
     title: "Website SME-HUB",
     category: ["web"],
@@ -21,20 +29,12 @@ export const projectsData = [
     stack: "Node.JS, React.JS"
   },
   {
-    id: 3,
+    id: 4,
     image: work3,
     title: "App Persebaran UMKM",
     category: ["app"],
     link: "https://github.com/erzajuan/persebaran_umkm",
     stack: "Flutter"
-  },
-  {
-    id: 4,
-    image: work4,
-    title: "Story App",
-    category: "app",
-    link: "https://github.com/erzajuan/StoryApp",
-    stack: "Kotlin"
   },
   
 ];
